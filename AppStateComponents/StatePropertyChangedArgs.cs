e9277029921aa19267faad0c760a822e48d28366
Carl Franklin <carl@appvnext.com>
2023-11-29 11:39:16 -0500

    Add project files.

---
+++ AppStateComponents/StatePropertyChangedArgs.cs
@@ -0,0 +1,6 @@
+﻿/// <summary>
+/// This record is used to pass the name of the property that changed and the new value of the property.
+/// </summary>
+/// <param name="PropertyName"></param>
+/// <param name="NewValue"></param>
+public record StatePropertyChangedArgs(string PropertyName, object? NewValue);
